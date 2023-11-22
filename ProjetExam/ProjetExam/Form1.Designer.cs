50ca78e6e07f20144f7a620535a8425ac155dd79
Brendan Bx <Brendanbx@gmail.com>
2023-11-22 16:30:13 +0100

    Jour 1
    connexion database et mise en place des combobox

--- ProjetExam/ProjetExam/Form1.Designer.cs
+++ ProjetExam/ProjetExam/Form1.Designer.cs
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox4 = new ComboBox();
+            comboBox5 = new ComboBox();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Location = new Point(324, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 77);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(169, 77);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(326, 77);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(151, 28);
+            comboBox3.TabIndex = 4;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(483, 77);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(151, 28);
+            comboBox4.TabIndex = 5;
+            // 
+            // comboBox5
+            // 
+            comboBox5.FormattingEnabled = true;
+            comboBox5.Location = new Point(640, 77);
+            comboBox5.Name = "comboBox5";
+            comboBox5.Size = new Size(151, 28);
+            comboBox5.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 118);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 7;
+            label3.Text = "label3";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(676, 171);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 8;
+            button1.Text = "Valider";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(comboBox5);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private ComboBox comboBox5;
+        private Label label3;
+        private Button button1;
     }
 }
--- ProjetExam/ProjetExam/Form1.cs
+++ ProjetExam/ProjetExam/Form1.cs
@@ -1,10 +1,158 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+
+        
 namespace ProjetExam
 {
     public partial class Form1 : Form
     {
+
+        private void recupDatabase(MySqlConnection connection)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM vehicules";
+            MySqlDataReader reader = command.ExecuteReader();
+
+            reader.Read();
+            string note = reader.GetString("Note eco");
+            label3.Text = note;
+
+            
+        }
+        private void afficherVehicule(MySqlConnection connection)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM vehicules";
+            MySqlDataReader reader = command.ExecuteReader();
+
+
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            comboBox1.DisplayMember = "Nom";
+            comboBox1.DataSource = dt;
+
+
+
+        }
+
+
+        private void afficherEnergie(MySqlConnection connection)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM energie";
+            MySqlDataReader reader = command.ExecuteReader();
+
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            comboBox2.DisplayMember = "Nom";
+            comboBox2.ValueMember = "Note eco";
+            comboBox2.DataSource = dt;
+
+        }
+
+        private void afficherKilometrage(MySqlConnection connection)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM kilometrage";
+            MySqlDataReader reader = command.ExecuteReader();
+
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            comboBox3.DisplayMember = "Nom";
+            comboBox3.ValueMember = "Note eco";
+            comboBox3.DataSource = dt;
+
+        }
+
+        private void afficherAnnee(MySqlConnection connection)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM annee";
+            MySqlDataReader reader = command.ExecuteReader();
+
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            comboBox4.DisplayMember = "Nom";
+            comboBox4.ValueMember = "Note eco";
+            comboBox4.DataSource = dt;
+
+        }
+
+        private void afficherPassager(MySqlConnection connection)
+        {
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM passager";
+            MySqlDataReader reader = command.ExecuteReader();
+
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            comboBox5.DisplayMember = "Nombre";
+            comboBox5.ValueMember = "Pourcentage";
+            comboBox5.DataSource = dt;
+
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder connectionStringBuilder = new MySqlConnectionStringBuilder
+            {
+                Server = "127.0.0.1",
+                UserID = "root",
+                Password = "",
+                Database = "projetcsharp"
+            };
+            try
+            {
+                // Tenter d'établir la connexion
+                MySqlConnection connection = new MySqlConnection(connectionStringBuilder.ToString());
+                connection.Open();
+                label2.Text = "Veuillez choisir vos données de votre véhicule";
+                afficherVehicule(connection);
+                afficherEnergie(connection);
+                afficherKilometrage(connection);
+                afficherAnnee(connection);
+                afficherPassager(connection);
+                recupDatabase(connection);
+
+
+
+            }
+            catch (MySqlException except)
+            {
+                label2.Text = $"{except}";
+            }
+            label1.Text = "Bienvenue sur le site Green Bank !";
+            label1.ForeColor = Color.Green;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
+
 }
+
